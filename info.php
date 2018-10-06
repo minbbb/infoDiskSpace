0e74371053dbb6fa8a8c5eeae86a386122592ac7
minbbb <maks4339@gmail.com>
2018-10-06 20:10:03 +0300

    Added variable to change the disk

--- info.php
+++ info.php
@@ -1,3 +1,6 @@
-всего: <?php echo shell_exec("df -h | grep /dev/sda1 | awk '{print $2}'"); ?><br/>
-использовано: <?php echo shell_exec("df -h | grep /dev/sda1 | awk '{print $3}'"); ?> ( <?php echo shell_exec("df -h | grep /dev/sda1 | awk '{print $5}'"); ?>)<br/>
-доступно: <?php echo shell_exec("df -h | grep /dev/sda1 | awk '{print $4}'"); ?><br/>
+<?php
+$disk = '/dev/sda1'; //drive about which we want to know information
+?>
+всего: <?php echo shell_exec("df -h | grep $disk | awk '{print $2}'"); ?><br/>
+использовано: <?php echo shell_exec("df -h | grep $disk | awk '{print $3}'"); ?> ( <?php echo shell_exec("df -h | grep $disk | awk '{print $5}'"); ?>)<br/>
+доступно: <?php echo shell_exec("df -h | grep $disk | awk '{print $4}'"); ?><br/>
